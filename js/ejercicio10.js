c568338bdaa433535d0663bff8992536b83540ff
GeroCossio <cossiogeronimo601@gmail.com>
2023-03-01 12:55:24 -0300

    Decimo y onceavo ejercicio terminado

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -6,9 +6,7 @@ if (numero % 2 === 0) {
 } else if (numero % 3 === 0) {
   document.write("El numero "+ numero + " es divisible por 3");
 } else if (numero % 5 === 0) {
-  document.write("El numero "+ numero + " es divisible por 5");
-} else if (numero % 7 === 0) {
-  document.write("El numero "+ numero + " es divisible por 7");
+  document.write("El numero "+ numero + " es divisible por 5"); 
 } else {
-  document.write("El numero "+ numero + " no es divisible por ningun numero :( ");
+    document.write("El numero "+ numero + " es divisible por 7");
 }
