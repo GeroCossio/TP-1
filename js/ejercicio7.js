a1a68ab4cc46a01f762bdb63e1b86f59d9c53886
GeroCossio <cossiogeronimo601@gmail.com>
2023-03-01 11:58:16 -0300

    Septimo ejercicio terminado

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,1 +1,14 @@
 //7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+let numero1 = prompt("Ingresa el primer número:");
+let numero2 = prompt("Ingresa el segundo número:");
+let numero3 = prompt("Ingresa el tercer número:");
+
+if (numero1 > numero2 && numero1 > numero3) {
+  document.write("El primer numero es el mas grande: " + numero1);
+} else if (numero2 > numero1 && numero2 > numero3) {
+  document.write("El segundo numero es el mas grande: " + numero2);
+} else if (numero3 > numero1 && numero3 > numero2) {
+  document.write("El tercer numero es el mas grande: " + numero3);
+} else {
+  document.write("Ningun numero es el mayor porque pusiste tres numeros iguales");
+}
