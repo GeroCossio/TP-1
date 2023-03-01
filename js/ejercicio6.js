da9b2ecab554f6c66e30e7854a36821f9b564d5a
GeroCossio <cossiogeronimo601@gmail.com>
2023-03-01 11:52:08 -0300

    Sexto ejercicio terminado

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,1 +1,11 @@
 //6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+let numero1 = parseInt(prompt("Por favor, ingresa el primer número:"));
+let numero2 = parseInt(prompt("Por favor, ingresa el segundo número:"));
+
+if (numero1 > numero2) {
+    document.write("El primer numero es el mas grande: " + numero1);
+  } else if (numero2 > numero1) {
+    document.write("El segundo numero es el mas grande " + numero2);
+  } else {
+    document.write("Hacelo de nuevo e ingresa dos numeros distintos :)");
+  }
